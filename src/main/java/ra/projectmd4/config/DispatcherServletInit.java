f6d3bb63007a16aeedb2e7aa3de253448f731639
hunghx <baby10051999@gmail.com>
2024-09-26 13:34:12 +0700

    up

---
+++ src/main/java/ra/projectmd4/config/DispatcherServletInit.java
@@ -0,0 +1,31 @@
+package ra.projectmd4.config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class DispatcherServletInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{HibernateConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        // cau hinh mvc
+        return new Class[]{MVCConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+    // caaus hinfh utf-8
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter("UTF-8",true);
+        return new Filter[]{filter};
+    }
+}
---
+++ src/main/java/ra/projectmd4/controller/Home.java
@@ -0,0 +1,12 @@
+package ra.projectmd4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+}
